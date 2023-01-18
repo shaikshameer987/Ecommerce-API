5ab33940ff9cd26d60997db7f8ae7ec189e904c1
Shaik Shameer Basha <shaikshameer987@gmail.com>
2023-01-18 19:50:45 +0530

    changed images field in product schema

--- models/products.js
+++ models/products.js
@@ -23,18 +23,7 @@ const productSchema = new mongoose.Schema({
         type: Number,
         default: 0
     },
-    images: [
-        {
-            public_id: {
-                type: String,
-                required: true
-            },
-            url: {
-                type: String,
-                required: true
-            }
-        }
-    ],
+    images: [String],
     category: {
         type: String,
         required: [true, "Please enter the product category"]
